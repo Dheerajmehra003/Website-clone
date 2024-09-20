adfbd1b70ba7f42ac98cc813061e00c6f9142a3b
Dheerajmehra003 <dheerajsinghmehra0405@gmail.com>
2024-09-20 17:04:15 +0530

    watchpage

--- Youtube/src/Components/VideoCart.jsx
+++ Youtube/src/Components/VideoCart.jsx
@@ -10,10 +10,8 @@ const VideoCart = ({item}) => {
     const getYoutubeChannelName = async() =>{
       try {
         const res = await axios.get(`https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${item.snippet.channelId}&key=${API_KEY}`)
-        console.log(res)
         setYtIcon(res.data.items[0].snippet.thumbnails.high.url)
       } catch (error) {
-      console.log(error)
       }
     }
 
