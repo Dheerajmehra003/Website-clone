687084dd4d44143bc6ff409cdc9b4971e37574bd
Dheerajmehra003 <dheerajsinghmehra0405@gmail.com>
2024-11-08 11:25:17 +0530

    login

--- Netflix/src/Components/TitleCards/TitleCards.jsx
+++ Netflix/src/Components/TitleCards/TitleCards.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './TitleCards.css'
+import card_data from '../../assets/cards/Cards_data'
 
 
-const TitleCards = () => {
+const TitleCards = ({title,category}) => {
+
+  const cardsRef = useRef()
+
+const handleWheel = (event) => {
+  event.preventDefault();
+  cardsRef.current.scrollLeft += event.deltaY;
+}
+
+useEffect(()=>{
+  cardsRef.current.addEventListener('wheel', handleWheel)
+},[])
+
   return (
-    <div>TitleCards</div>
+    <div className="titleCards">
+      <h2>{title ? title : 'Popular on Netflix'}</h2>
+      <div className="cardList" ref={cardsRef}>
+        {
+          card_data.map((item,index)=>{
+            return(
+              <div className="card" key={index}>
+                <img src={item.image} alt="" />
+                <p>{item.name}</p>
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
   )
 }
 
--- Netflix/src/Pages/Login/Login.jsx
+++ Netflix/src/Pages/Login/Login.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import './Login.css'
+import logo from '../../assets/logo.png'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="login">
+      <img src={logo} className='loginLogo' alt="" />
+       <div className="loginForm">
+        <h1>Sign In</h1>
+        <form action="">
+          <input type="text" name='name' placeholder='Username' />
+          <input type="text" name='email' placeholder='Email' />
+          <input type="password"  placeholder='Password'/>
+          <button>Sign Up</button>
+          <div className="formHelp">
+            <div className="remember">
+              <input type="checkbox" />
+              <label htmlFor="">Remember Me</label>
+            </div>
+            <p>Need Help?</p>
+          </div>
+        </form>
+        <div className="formSwitch">
+          <p>New to Netflix? <span>Sign Up Now</span></p>
+          <p>Already have account? <span>Sign In Now</span></p>
+        </div>
+       </div>
+    </div>
   )
 }
 
-export default Login
+export default Login;
