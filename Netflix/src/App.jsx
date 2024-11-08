88e33dc3920407d0961c6cecfad83a1cc6fb9da2
Dheerajmehra003 <dheerajsinghmehra0405@gmail.com>
2024-11-08 11:24:55 +0530

    footer

--- Netflix/src/App.jsx
+++ Netflix/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Home from './Pages/Home/Home'
+import { Route, Routes } from 'react-router-dom'
+import Login from './Pages/Login/Login'
 
 const App = () => {
   return (
     <>
-    <Home />
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/login' element={<Login />}/>
+    </Routes>
     </>
   )
 }
--- Netflix/src/Pages/Home/Home.jsx
+++ Netflix/src/Pages/Home/Home.jsx
@@ -5,9 +5,12 @@ import heroBanner from '../../../public/hero_banner.jpg'
 import heroTitle from '../../assets/hero_title.png'
 import info from '../../assets/info_icon.png'
 import play from '../../assets/play_icon.png'
+import TitleCards from '../../Components/TitleCards/TitleCards'
+import Footer from '../../Components/Footer/Footer'
 
 const Home = () => {
   return (
+    <>
     <div className='home'>
       <NavigationBar />
       <div className="hero">
@@ -22,9 +25,18 @@ const Home = () => {
             <button className='btn'><img src={play} alt="" />Play</button>
             <button className='btn darkbtn'><img src={info} alt="" />More Info</button>
           </div>
+          <TitleCards  />
         </div>
       </div>
     </div>
+    <div className="moreCards">
+      <TitleCards  title={'BlockBuster Movies'} />
+      <TitleCards  title={'Only on Netflix'} />
+      <TitleCards  title={'Upcoming'} />
+      <TitleCards  title={'Top Picks for You'} />
+    </div>
+    <Footer />
+    </>
   )
 }
 
